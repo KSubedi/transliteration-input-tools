6ec0525b08b718af67ae270840998b6415aa44de
Kaushal Subedi <kaushal@wireshock.com>
2018-12-09 21:43:22 -0700

    added index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+import { enableTransliteration, disableTransliteration } from "./dist/transliteration-input.bundle";
+
+export function enableTransliteration;
+export function disableTransliteration;
